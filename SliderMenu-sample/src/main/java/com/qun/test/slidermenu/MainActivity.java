f61a2633c7d27f638140c9e3440c43745a71e899
weimcgrady91 <331855608@qq.com>
2018-04-02 16:14:01 +0800

    Initial commit

---
+++ SliderMenu-sample/src/main/java/com/qun/test/slidermenu/MainActivity.java
@@ -0,0 +1,46 @@
+package com.qun.test.slidermenu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.qun.lib.slidermenu.SliderMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SliderMenu mSliderMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        mSliderMenu = findViewById(R.id.slider_menu);
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSliderMenu.getState() == SliderMenu.SliderMenuState.CLOSE) {
+                    mSliderMenu.changedState(SliderMenu.SliderMenuState.OPEN);
+                } else {
+                    mSliderMenu.changedState(SliderMenu.SliderMenuState.CLOSE);
+                }
+            }
+        });
+        ListView listView = findViewById(R.id.lv);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getData()));
+    }
+
+    public List<String> getData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            data.add("这是第" + i + "个条目");
+        }
+        return data;
+    }
+}
---
+++ slidermenu/src/main/java/com/qun/lib/slidermenu/SliderMenu.java
@@ -0,0 +1,139 @@
+package com.qun.lib.slidermenu;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * Created by Administrator on 2018/4/2 0002.
+ */
+
+public class SliderMenu extends ViewGroup {
+
+    private View mSliderMenu;
+    private View mMainContent;
+    private Scroller mScroller;
+    private SliderMenuState mState = SliderMenuState.CLOSE;
+
+    public SliderMenuState getState() {
+        return mState;
+    }
+
+    public enum SliderMenuState {
+        OPEN, CLOSE
+    }
+
+    public SliderMenu(Context context) {
+        this(context, null);
+    }
+
+    public SliderMenu(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SliderMenu(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mScroller = new Scroller(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        mSliderMenu = getChildAt(0);
+        mMainContent = getChildAt(1);
+        setMeasuredDimension(mMainContent.getMeasuredWidth(),
+                mSliderMenu.getMeasuredHeight() >= mMainContent.getMeasuredHeight() ?
+                        mSliderMenu.getMeasuredHeight() : mMainContent.getMeasuredHeight());
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mSliderMenu.layout(-mSliderMenu.getMeasuredWidth(), 0, 0, b);
+        mMainContent.layout(l, t, r, b);
+    }
+
+    private int mLastX;
+    private int mLastY;
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastX = (int) ev.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int curX = (int) ev.getX();
+                if (Math.abs(curX - mLastX) > 20) {
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastX = (int) event.getX();
+                mLastY = (int) event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int curX = (int) event.getX();
+                int curY = (int) event.getY();
+                int dx = curX - mLastX;
+
+                if (getScrollX() - dx <= -mSliderMenu.getWidth()) {
+                    scrollTo(-mSliderMenu.getWidth(), 0);
+                } else if (getScrollX() - dx >= 0) {
+                    scrollTo(0, 0);
+                } else {
+                    scrollBy(-dx, 0);
+                }
+                mLastX = curX;
+                mLastY = curY;
+                break;
+            case MotionEvent.ACTION_UP:
+                if (getScrollX() <= -mSliderMenu.getWidth() / 3) {
+                    mScroller.startScroll(getScrollX(), 0, -(mSliderMenu.getWidth() - Math.abs(getScrollX())), 0);
+                    invalidate();
+                    mState = SliderMenuState.OPEN;
+                } else {
+                    mScroller.startScroll(getScrollX(), 0, -getScrollX(), 0);
+                    invalidate();
+                    mState = SliderMenuState.CLOSE;
+                }
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), 0);
+            postInvalidate();
+        }
+    }
+
+    public void changedState(SliderMenuState state) {
+        if (!mScroller.isFinished()) {
+            mScroller.abortAnimation();
+        }
+        if (mState == SliderMenuState.OPEN) {
+            mScroller.startScroll(getScrollX(), 0, -getScrollX(), 0);
+        } else {
+            mScroller.startScroll(getScrollX(), 0, -(mSliderMenu.getWidth() - Math.abs(getScrollX())), 0);
+        }
+        invalidate();
+        mState = state;
+    }
+}
